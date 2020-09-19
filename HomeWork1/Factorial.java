a9d5369bfa990d712f8e831977136c5c4d26c9e2
Saiful Alam <saiful.rakib@dh.health>
2020-09-19 18:09:01 +0600

    Homework done

--- HomeWork1/Factorial.java
+++ HomeWork1/Factorial.java
@@ -1,10 +1,45 @@
 import java.util.*;
 
 public class Factorial {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
-        
-        // Operations 
+        int number = scanner.nextInt();
+        // Operations
+
+        Factorial instance = new Factorial();
+        System.out.print("Choose Action (1: using for loop, 2: using while loop): ");
+        int action = scanner.nextInt();
+        if (action == 1) {
+            instance.usingForLoop(number);
+        } else {
+            instance.usingWhileLoop(number);
+        }
     }
+
+    public void usingForLoop(int number) {
+        if (number < 0)
+            System.out.println("Error! Factorial of a negative number doesn't exist.");
+        else {
+            int factorial = 1;
+            for (int i = 1; i <= number; ++i) {
+                factorial *= i;
+            }
+            System.out.println("Factorial of " + number + " is " + factorial);
+        }
+    }
+
+    public void usingWhileLoop(int number) {
+        if (number < 0)
+            System.out.println("Error! Factorial of a negative number doesn't exist.");
+        else {
+            int i = 1, factorial = 1;
+            while (i <= number) {
+                factorial *= i;
+                i++;
+            }
+            System.out.println("Factorial of " + number + " is " + factorial);
+        }
+    }
+
 }
--- HomeWork1/Fibonacci.java
+++ HomeWork1/Fibonacci.java
@@ -1,10 +1,45 @@
 import java.util.*;
 
 public class Fibonacci {
-    public static void main(String []args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
-        
-        // Operations 
+        int number = scanner.nextInt();
+        // Operations
+        Fibonacci instance = new Fibonacci();
+        System.out.print("Choose Action (1: using for loop, 2: using while loop): ");
+        int action = scanner.nextInt();
+        if (action == 1) {
+            instance.usingForLoop(number);
+        } else {
+            instance.usingWhileLoop(number);
+        }
+    }
+
+    public void usingForLoop(int counter) {
+        int i, num1 = 0, num2 = 1, result;
+
+        System.out.print(num1 + " " + num2);
+        for (i = 2; i < counter; ++i) {
+            result = num1 + num2;
+            System.out.print(" " + result);
+            num1 = num2;
+            num2 = result;
+        }
+        System.out.println(" ");
+    }
+
+    public void usingWhileLoop(int counter) {
+        int i = 2, num1 = 0, num2 = 1, result;
+
+        System.out.print(num1 + " " + num2);
+        while (i < counter) {
+            result = num1 + num2;
+            System.out.print(" " + result);
+            num1 = num2;
+            num2 = result;
+            i++;
+        }
+        System.out.println(" ");
     }
 }
